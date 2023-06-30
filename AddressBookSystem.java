9bf6a96a73b0ecfc19d6043be910fe81a05e9dc4
AsmitaSubh <saswain92@gmail.com>
2023-06-30 18:30:30 +0530

    AddressBookSystem

---
+++ AddressBookSystem.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+class Contact {
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
+
+class AddressBook {
+    private List<Contact> contacts;
+
+    public AddressBook() {
+        contacts = new ArrayList<>();
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+        System.out.println("Contact added: " + contact.getName());
+    }
+
+    public void displayContacts() {
+        if (contacts.isEmpty()) {
+            System.out.println("Address book is empty.");
+            return;
+        }
+
+        System.out.println("Address Book:");
+        for (Contact contact : contacts) {
+            System.out.println("Name: " + contact.getName());
+            System.out.println("Phone Number: " + contact.getPhoneNumber());
+            System.out.println("-----------------------");
+        }
+    }
+}
+
+public class AddressBookSystem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        AddressBook addressBook = new AddressBook();
+
+        while (true) {
+            System.out.println("Address Book System");
+            System.out.println("-------------------");
+            System.out.println("1. Add Contact");
+            System.out.println("2. Display Contacts");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter phone number: ");
+                    String phoneNumber = scanner.nextLine();
+                    Contact contact = new Contact(name, phoneNumber);
+                    addressBook.addContact(contact);
+                    break;
+                case 2:
+                    addressBook.displayContacts();
+                    break;
+                case 3:
+                    System.out.println("Exiting the program. Goodbye!");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+
+            System.out.println();
+        }
+    }
+}
